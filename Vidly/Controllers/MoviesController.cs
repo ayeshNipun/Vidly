e120d11167433ebac0d5db4f023b5fcf8ec9c4ca
ayeshNipun <nipun.yesh@gmail.com>
2019-05-30 09:25:56 +0530

    pure view model for movie class

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -42,7 +42,6 @@ namespace Vidly.Controllers
 
             var viewModel = new MovieFormViewModel
             {
-                Movie = new Movie(),
                 Genres = genreTypes
             };
 
@@ -54,9 +53,8 @@ namespace Vidly.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var viewModel = new MovieFormViewModel
+                var viewModel = new MovieFormViewModel(movie)
                 {
-                    Movie = movie,
                     Genres = _context.Genres.ToList()
                 };
 
@@ -91,9 +89,9 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
+                
                 Genres = _context.Genres.ToList()
             };
 
--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -9,6 +10,39 @@ namespace Vidly.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+       
+        [Display(Name = "Genre")]
+        public byte? GenreId { get; set; }
+
+        [Display(Name = "Released Date")]
+        [Required]
+        public DateTime? ReleasedDate { get; set; }
+
+        [Display(Name = "Number is stock")]
+        [Range(1, 20, ErrorMessage = "Amount should be between 1 and 20")]
+        [Required]
+        public int? NumberInStock { get; set; }
+
+        //for new movie
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        //for edit movie
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleasedDate = movie.ReleasedDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
     }
 }
