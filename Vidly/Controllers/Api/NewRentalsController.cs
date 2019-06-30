f1057843d17d78445722abd523e167374dc60b2d
ayeshNipun <nipun.yesh@gmail.com>
2019-06-30 09:16:01 +0530

    added validations for movie availability

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -22,10 +22,16 @@ namespace Vidly.Controllers.Api
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
             var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+//           if (movies.Count != newRental.MovieIds.Count)
+//                return BadRequest("One or more movies are invalid");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
                 movie.NumberAvailable--;  
                 var rental = new Rental
                 {
